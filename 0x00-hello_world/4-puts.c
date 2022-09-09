bd77fc1d6943fc2146ad99721dc435baf18107cf
Tensae Teji <tensaeteji03@gmail.com>
2022-09-09 10:46:47 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* puts
-*
-* printf
-* return: 0
+* main - print the string in the put function
+* 
+* Description: using the main function
+* Return: 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0):
 }
