18d287e9170c98301b08fef9c94df7744540f541
Tensae Teji <tensaeteji03@gmail.com>
2022-09-09 12:44:38 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the write function
+ *
+ * Description: using the main function
+ * this program print and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * Return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -9,3 +9,5 @@
 
 7. Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file.
 
+Write a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+
