2c5c04f358cc50d9c1597cd88b17fecff12e2fa0
Tensae Teji <tensaeteji03@gmail.com>
2022-09-12 07:09:12 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print commbination of two single digit numbers
+ *
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -18,3 +18,5 @@
 
 8. Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.
 
+9. Write a program that prints all possible combinations of single-digit numbers.
+
