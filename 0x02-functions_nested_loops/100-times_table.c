68b9018ff5a8e9ebf84e10b927b4b2beae889871
Tensae Teji <tensaeteji03@gmail.com>
2022-09-18 04:00:31 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints n times table
+ *
+ * @n: n times
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	print_times_table(-5);
+	_putchar('\n');
+	print_times_table(-4);
+	_putchar('\n');
+	print_times_table(-3);
+	_putchar('\n');
+	print_times_table(-2);
+	_putchar('\n');
+	print_times_table(-1);
+	_putchar('\n');
+	print_times_table(16);
+	_putchar('\n');
+	print_times_table(17);
+	_putchar('\n');
+	print_times_table(18);
+	_putchar('\n');
+	print_times_table(19);
+	_putchar('\n');
+	print_times_table(n >= 20);
+	_putchar('\n');
+}
