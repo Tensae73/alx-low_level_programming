c5882ea01d45c5aa433a67d2ff0d74d849750df9
Tensae Teji <tensaeteji03@gmail.com>
2022-10-20 05:42:32 -0700

    header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,5 +2,10 @@
 #define LISTS_H	
 
 char _putchar(char);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
